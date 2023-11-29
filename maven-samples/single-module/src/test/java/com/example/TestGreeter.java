1ed2ea939c44deedbc5ae6580205a50299411622
AKSHAY <acharyaakshay445@gmail.com>
2023-11-29 19:22:40 +0530

    Removed a test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,9 +41,4 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-
- @Test
-  public void Intentionally_Failed() {
-    assertTrue(true);
-  }
 }
