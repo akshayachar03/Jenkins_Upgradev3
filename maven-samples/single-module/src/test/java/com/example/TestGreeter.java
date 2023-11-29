461f2e25f870846da51097157f04348685564d7c
AKSHAY <acharyaakshay445@gmail.com>
2023-11-29 18:56:18 +0530

    Expected error commit

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -41,8 +41,9 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
-}
+
  @Test
   public void greetShouldIncludeGreetingPhrase() {
     assertTrue(false);
   }
+}
